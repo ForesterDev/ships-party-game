c6c9445d8bfbcdf4de544c960fd877adc26a035e
Javier Cañadilla Casco <jcanadillacasco@outlook.com>
2021-01-31 00:48:43 +0100

    Configuracion básica del proyecto. Pantalla principal completada. Pantalla de crear partida comenzada.

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour
+{
+
+    private string CREATE_GAME_SCENE = "CreateGame";
+    private string SEARCH_GAME_SCENE = "SearchGame";
+    private string SETTINGS_SCENE = "Settings";
+
+    private void Awake()
+    {
+    }
+
+    public void OnCreateGame()
+    {
+        SceneManager.LoadScene(CREATE_GAME_SCENE, LoadSceneMode.Single);
+    }
+
+    public void OnSearchGame()
+    {
+        SceneManager.LoadScene(SEARCH_GAME_SCENE, LoadSceneMode.Single);
+    }
+
+    public void OnSettings()
+    {
+        SceneManager.LoadScene(SETTINGS_SCENE, LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/UI/ToggleButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ToggleButtons : MonoBehaviour
+{
+
+    [SerializeField]
+    protected TextMeshPro[] buttonsTMPro;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnSelected()
+    {
+
+    }
+
+}
